b450a0889566d4327f8f351173af442a555e7f49
ShadowOfHumanity <Darianbakerbray@gmail.com>
2024-09-02 09:55:42 +0200

    important implementation

--- src/main/java/net/Darian/toLearnMod/ToLearnMod.java
+++ src/main/java/net/Darian/toLearnMod/ToLearnMod.java
@@ -20,7 +20,7 @@ public class ToLearnMod
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "to_learn_mod";
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger()
+    private static final Logger LOGGER = LogUtils.getLogger();
     public ToLearnMod(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
 
