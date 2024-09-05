f0a99f7babac2bd1b25c58bd803028dcdeb48da1
ShadowOfHumanity <Darianbakerbray@gmail.com>
2024-09-05 12:36:23 +0200

    - Added new mod universal creative tab   - "mod_tab"

--- src/main/java/net/Darian/toLearnMod/item/ModCreativeModeTabs.java
+++ src/main/java/net/Darian/toLearnMod/item/ModCreativeModeTabs.java
@@ -14,6 +14,31 @@ public class ModCreativeModeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
         DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ToLearnMod.MOD_ID);
 
+    public static final RegistryObject<CreativeModeTab> ModTab = CREATIVE_MODE_TABS.register("mod_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(Items.WIND_CHARGE.asItem())) // the new item stack code is the icon of the creative tab
+                    .title(Component.translatable("creativetab.mod_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.FireSword.get());
+                        output.accept(ModItems.PoisonSword.get());
+
+                        output.accept(ModItems.Sapphire.get());
+                        output.accept(ModItems.Raw_Sapphire.get());
+                        output.accept(ModItems.Pinecone.get());
+                        output.accept(ModItems.Strawberry.get());
+
+                        output.accept(ModItems.Metal_Detector.get());
+                        output.accept(ModBlocks.Sound_Block.get());
+
+                        output.accept(ModBlocks.Sapphire_Block.get());
+                        output.accept(ModBlocks.Raw_Sapphire_Block.get());
+
+                        output.accept(ModBlocks.Raw_Sapphire_Ore.get());
+                        output.accept(ModBlocks.Deepslate_Sapphire_Ore.get());
+                        output.accept(ModBlocks.Nether_Sapphire_Ore.get());
+                        output.accept(ModBlocks.End_Sapphire_Ore.get());
+                    })
+                    .build());
+
     public static final RegistryObject<CreativeModeTab> ModSwords = CREATIVE_MODE_TABS.register("sword_tab",
             () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.FireSword.get())) // the new item stack code is the icon of the creative tab
                     .title(Component.translatable("creativetab.sword_tab"))
@@ -60,30 +85,6 @@ public class ModCreativeModeTabs {
                     })
                     .build());
 
-    public static final RegistryObject<CreativeModeTab> ModTab = CREATIVE_MODE_TABS.register("mod_tab",
-            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.Sapphire.get())) // the new item stack code is the icon of the creative tab
-                    .title(Component.translatable("creativetab.mod_tab"))
-                    .displayItems((itemDisplayParameters, output) -> {
-                        output.accept(ModItems.FireSword.get());
-                        output.accept(ModItems.PoisonSword.get());
-
-                        output.accept(ModItems.Sapphire.get());
-                        output.accept(ModItems.Raw_Sapphire.get());
-                        output.accept(ModItems.Pinecone.get());
-                        output.accept(ModItems.Strawberry.get());
-
-                        output.accept(ModItems.Metal_Detector.get());
-                        output.accept(ModBlocks.Sound_Block.get());
-
-                        output.accept(ModBlocks.Sapphire_Block.get());
-                        output.accept(ModBlocks.Raw_Sapphire_Block.get());
-
-                        output.accept(ModBlocks.Raw_Sapphire_Ore.get());
-                        output.accept(ModBlocks.Deepslate_Sapphire_Ore.get());
-                        output.accept(ModBlocks.Nether_Sapphire_Ore.get());
-                        output.accept(ModBlocks.End_Sapphire_Ore.get());
-                    })
-                    .build());
 
 
 
